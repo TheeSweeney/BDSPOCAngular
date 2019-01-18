9f9498e6ec6a63d45b7a1287d8e671a3b6d40ff6
Sweeney <brian.sweeney@ibm.com>
2019-01-18 10:38:34 -0500

    components for doc-upload/list-docs created; "data" moved to it's own file;

--- BDS-UI/src/app/app.module.ts
+++ BDS-UI/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DocUploadComponent } from './doc-upload/doc-upload.component';
+import { ListDocsComponent } from './list-docs/list-docs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocUploadComponent,
+    ListDocsComponent
   ],
   imports: [
     BrowserModule
