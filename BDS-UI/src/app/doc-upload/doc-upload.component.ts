e95f267ff31c6118c73650f60669ec461f499408
Sweeney <brian.sweeney@ibm.com>
2019-01-25 15:31:31 -0500

    upload skeleton in place;

--- BDS-UI/src/app/doc-upload/doc-upload.component.ts
+++ BDS-UI/src/app/doc-upload/doc-upload.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 
 @Component({
@@ -9,7 +10,25 @@ import { catchError, retry } from 'rxjs/operators';
 })
 export class DocUploadComponent implements OnInit {
 
+  doc: any;
+
   constructor(private http: HttpClient) { }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -23,8 +42,8 @@ export class DocUploadComponent implements OnInit {
   ngOnInit() {
   }
 
-  handleFileInput(files){
-     
+  handleFileInput(uploadDoc){
+    this.doc = uploadDoc
   }
 
   uploadFile = function(doc: any){
